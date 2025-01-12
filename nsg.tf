536c2757327bb7f52017ea53a6e768b1d62f5364
Jasper-Thanapal <144192820+Jasper-Thanapal@users.noreply.github.com>
2025-01-12 18:22:46 +0530

    Add files via upload

---
+++ nsg.tf
@@ -0,0 +1,45 @@
+variable "azurerm_resource_group_name" {
+  type = string
+}
+
+variable "azurerm_resource_group_location" {
+  type = string
+}
+
+variable "nsgname" {
+  type = string
+}
+
+# Create Network Security Group and rule
+resource "azurerm_network_security_group" "myNSG" {
+  name                = var.nsgname
+  location            = var.azurerm_resource_group_location
+  resource_group_name = var.azurerm_resource_group_name
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+  security_rule {
+    name                       = "HTTP"
+    priority                   = 1000
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+output "security_rule" {
+  value = azurerm_network_security_group.myNSG.security_rule
+
+}
